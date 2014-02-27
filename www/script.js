e90b26d98383ea06e2880f60f4984e31bd76764a
paulktp <paul@ktp.net>
2014-02-27 16:52:26 +0100

    4

--- www/script.js
+++ www/script.js
@@ -1,6 +1,37 @@
+// Global InAppBrowser reference
+var iabRef = null;
+
 function onDeviceReady() {
 	
-	$("#app-status-ul").append('<li>deviceready event received</li>');
+	//$("#app-status-ul").append('<li>deviceready event received</li>');
+	
+	iabRef = window.open('http://wp1.ktp-concept.com/', '_blank', 'location=no');
+    iabRef.addEventListener('loadstart', iabLoadStart);
+    iabRef.addEventListener('loadstop', iabLoadStop);
+    iabRef.removeEventListener('loaderror', iabLoadError);
+    iabRef.addEventListener('exit', iabClose);
+
 }
+
+function iabLoadStart(event) {
+    //alert(event.type + ' - ' + event.url);
+}
+
+function iabLoadStop(event) {
+    //alert(event.type + ' - ' + event.url);
+}
+
+function iabLoadError(event) {
+    //alert(event.type + ' - ' + event.message);
+}
+
+function iabClose(event) {
+     //alert(event.type);
+     iabRef.removeEventListener('loadstart', iabLoadStart);
+     iabRef.removeEventListener('loadstop', iabLoadStop);
+     iabRef.removeEventListener('loaderror', iabLoadError);
+     iabRef.removeEventListener('exit', iabClose);
+}
+
 
 document.addEventListener('deviceready', onDeviceReady, true);
